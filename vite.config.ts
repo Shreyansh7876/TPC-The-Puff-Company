b210b39140e4271d15bd0b5d348aa07d8596ab69
Shreyansh7876 <shubhampatel530@gmail.com>
2026-08-29 17:19:17 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: '/',
+    base: '/TPC-The-Puff-Company/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
